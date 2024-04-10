4850b9451b7fee7287e0b71c20e93cbc033b72fa
rmac3392 <ryanjamesmacawili@gmail.com>
2024-04-10 22:40:05 +0800

    Lezzgow

--- src/MainPages/Home.vue
+++ src/MainPages/Home.vue
@@ -5,7 +5,22 @@
     thirdLabel="DummyLabel"
     fourthLabel="DummyLabel"
   />
-  <div>dadaddadadad</div>
+  <button class="btn btn-primary">Click me</button>
+  <button class="btn bg-red-400" onclick="my_modal_1.showModal()">
+    open modal
+  </button>
+  <dialog id="my_modal_1" class="modal">
+    <div class="modal-box">
+      <h3 class="font-bold text-lg">Hello!</h3>
+      <p class="py-4">Press ESC key or click the button below to close</p>
+      <div class="modal-action">
+        <form method="dialog">
+          <!-- if there is a button in form, it will close the modal -->
+          <button class="btn">Close</button>
+        </form>
+      </div>
+    </div>
+  </dialog>
 </template>
 <script setup>
 import NavBar from "../components/NavBar.vue";
