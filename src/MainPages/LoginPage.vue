f9e33ef7eca331be834dafb569a3b268e4350cc8
Michael Tonilon <mtonilon17@gmail.com>
2024-04-10 10:06:23 +0800

    updated login ui

--- src/MainPages/LoginPage.vue
+++ src/MainPages/LoginPage.vue
@@ -3,7 +3,7 @@
     <div><NavBarLanding firstLabel="PointQuest" /></div>
 
     <div class="w-[100%] items-center justify-center flex h-[700px]">
-      <div class="w-[40%] ml-[5%] text-primary font-semibold tracking-widest">
+      <div class="w-[40%] ml-[5%] text-secondary font-semibold tracking-widest">
         <img :src="image" class="w-[100%] h-[100%] float-anim" />
       </div>
       <div class="w-[40%] ml-[10%]">
@@ -27,11 +27,11 @@
                     >Username</label
                   >
                   <input
-                  v-model="username"
+                    v-model="username"
                     type="username"
                     name="email"
                     id="email"
-                    class="bg-gray-50 border font-500 border-gray-300 text-primary sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-primary dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    class="bg-primary border font-500 border-secondary text-secondary sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="MTonilon22"
                     required=""
                   />
@@ -43,12 +43,12 @@
                     >Password</label
                   >
                   <input
-                  v-model="password"
+                    v-model="password"
                     type="password"
                     name="password"
                     id="password"
                     placeholder="••••••••"
-                    class="bg-gray-50 border font-500 border-gray-300 text-primary sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-primary dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    class="bg-primary border font-500 border-secondary text-secondary sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required=""
                   />
                 </div>
@@ -59,7 +59,7 @@
                         id="remember"
                         aria-describedby="remember"
                         type="checkbox"
-                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                        class="w-4 h-4 border border-secondary rounded bg-primary focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
                         required=""
                       />
                     </div>
@@ -68,25 +68,24 @@
                         >Remember me</label
                       >
                     </div>
-                    
                   </div>
                   <a href="#" class="text-sm text-ternary hover:underline"
                     >Forgot password?</a
                   >
                 </div>
-                  <button 
-                    @click="login"
-                    type="submit"
-                    class="w-[30%] btn transition duration-300 mt-8 text-p text-secondary font-500 tracking-wide bg-primary-600 focus:ring-primary-300 rounded-full text-sm px-5 py-2.5 text-center bg-ternary"
-                  >
-                    Sign in
-                  </button>
+                <button
+                  @click="login"
+                  type="submit"
+                  class="w-[30%] btn transition duration-300 mt-8 text-p text-secondary font-500 tracking-wide bg-primary-600 focus:ring-primary-300 rounded-full text-sm px-5 py-2.5 text-center bg-ternary"
+                >
+                  Sign in
+                </button>
 
                 <p class="text-sm font-light text-secondary">
                   Don’t have an account yet?
                   <router-link to="/register">
                     <a
-                      class="font-medium text-ternary hover:underline dark:text-primary-500 cursor-pointer"
+                      class="font-medium text-ternary hover:underline -500 cursor-pointer"
                       >Register</a
                     >
                   </router-link>
@@ -105,36 +104,28 @@
 import NavBarLanding from "../components/NavBarLanding.vue";
 import Footer from "../components/Footer.vue";
 import image from "../assets/register.png";
-import {ref} from "vue";
-import {useRouter} from "vue-router"
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 
 const username = ref();
 const password = ref();
 const router = useRouter();
 
-const login = async () =>{
-  try{
-    const response = await fetch ('http://localhost:8080/getUsers');
+const login = async () => {
+  try {
+    const response = await fetch("http://localhost:8080/getUsers");
     const data = await response.json();
-    for(var i = 0 ; i < data.length;i++){
-      if(data[i].username==username.value){
-        if(data[i].password == password.value){
-          router.push('/home');
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].username == username.value) {
+        if (data[i].password == password.value) {
+          router.push("/home");
+        } else {
         }
-        else{
-          
-        }
-      }
-      else{
-        
+      } else {
       }
     }
-
-
-  }
-  catch(error){
+  } catch (error) {
     console.log(error);
   }
-}
-
+};
 </script>
